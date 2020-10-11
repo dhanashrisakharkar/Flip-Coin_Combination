db9b9f04a5b22f65abd7f5af22bfc09d064e15f8
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-12 00:42:09 +0530

    Added : Flip Coin Head Or Taill

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
-echo "Welcome To Flip Coin Combination"
+Coin=$((1+RANDOM%3))
+
+if [ $Coin -eq 1 ]
+then
+     echo "It Is a HEAD"
+elif [ $Coin -eq 2 ]
+then
+     echo "It Is a TAIL"
+else
+    echo "It Is not Valid"
+fi
