62b03b277d98a88060886d1bd715fcc8f4e9fcf8
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-12 01:45:20 +0530

    Added : Double Combination Percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,39 +1,61 @@
 #!/bin/bash -x
 
-Headc=1
-Tailc=2
-Head=0
-Tail=0
+HHc=11
+TTc=22
+HTc=12
+THc=21
+HH=0
+TT=0
+HT=0
+TH=0
 
 declare -A Combi
 for ((i=1; i<30; i++))
 do
 
-Coin=$((1+RANDOM%2))
+Coin1=$((1+RANDOM%2))
+Coin2=$((1+RANDOM%2))
+double=$Coin1$Coin2
 
-if [ $Coin -eq $Headc ]
-then
-       ((Head++))
-
-elif [ $Coin -eq $Tailc ]
-then
-         ((Tail++))
-fi
-
+case $double in
+              11 )
+                    echo " It is a HH "
+                    ((HH++))
+                     ;;
+              12 )
+                    echo " It is a HT "
+                    ((HT++))
+                     ;;
+              21 )
+                    echo " It is a HT "
+                    ((TH++))
+                     ;;
+              22 )
+                    echo " It is a TT "
+                    ((TT++))
+                     ;;
+esac
 done
 
-Combi[Headc]=$Head
-Combi[Tailc]=$Tail
-
+Combi[HHc]=$HH
+Combi[TTc]=$TT
+Combi[HTc]=$HT
+Combi[THc]=$TH
 
 echo "Dictionary of Coin is " : ${Combi[@]}
 echo "Index of Dictionary is ": ${!Combi[@]}
 echo "Lenght Of Dictionary is": ${#Combi[@]}
 
+echo "HH Comes $HH Times"
+echo "TT comes $TT Times"
 
-HeadPer=`echo $Head | awk '{print ($1/30)*100}'`
-TailPer=`echo $Tail | awk '{print ($1/30)*100}'`
 
-echo "percentage of Head Comes : $HeadPer%"
-echo "Percentage of Tail Comes : $TailPer%"
+HHPer=`echo $HH | awk '{print ($1/30)*100}'`
+TTPer=`echo $TT | awk '{print ($1/30)*100}'`
+HTPer=`echo $HT | awk '{print ($1/30)*100}'`
+THPer=`echo $TH | awk '{print ($1/30)*100}'`
 
+echo "percentage of Double Head Comes : $HHPer%"
+echo "Percentage of Double Tail Comes : $TTPer%"
+echo "percentage of Double Head Comes : $HHPer%"
+echo "Percentage of Double Tail Comes : $TTPer%"
